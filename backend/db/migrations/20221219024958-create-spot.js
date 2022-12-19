1d5ea28356af33ecd0e239311499309c691e96b5
Peter Nguyen <nguyen.peter.viet@gmail.com>
2022-12-19 13:57:18 -0800

    test

--- backend/db/migrations/20221219024958-create-spot.js
+++ backend/db/migrations/20221219024958-create-spot.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -64,6 +70,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = "Spots";
+    return queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/20221219030242-create-review.js
+++ backend/db/migrations/20221219030242-create-review.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -46,6 +52,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    options.tableName = 'Reviews';
+    await queryInterface.dropTable(options);
   }
 };
