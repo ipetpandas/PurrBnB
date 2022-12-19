2112b9c25e95b190048d0ac423c72ad4ef49ebaa
Peter Nguyen <nguyen.peter.viet@gmail.com>
2022-12-18 20:45:18 -0800

    finished seeders

---
+++ backend/db/seeders/20221219043822-demo-spot-image.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('SpotImages', [
+      {
+        spotId: 1,
+        url: 'https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://images.pexels.com/photos/1974596/pexels-photo-1974596.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://images.pexels.com/photos/14613395/pexels-photo-14613395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        preview: true
+      },
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
